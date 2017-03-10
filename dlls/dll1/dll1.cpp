ee12f37d65febde37eca3f4b6a7a53ebff42d87f
Changju Rhee <changju.rhee@gmail.com>
2017-03-11 05:18:19 +0900

    Build success.

--- dlls/dll1/dll1.cpp
+++ dlls/dll1/dll1.cpp
@@ -15,20 +15,22 @@ const char* dll1::getText()
 {
     lib1 iLib1;
     
-    std::unique_ptr<int> p{new int{7}};
+    //std::unique_ptr<int> p{new int{7}};
     
-    p = iLib1.f(std::move(p));
+    //p = iLib1.f(std::move(p));
     
-    iLib1.f2(p);
+    //iLib1.f2(p);
     
-    Less_than<int> lti{42};
-    Less_than<std::string> lts{"bask"};
+    //Less_than<int> lti{42};
+    //Less_than<std::string> lts{"bask"};
     
-    bool b1 = lti(4);
-    bool b2 = lts("555");
+    //bool b1 = lti(4);
+    //bool b2 = lts("555");
     
     std::thread t(hello);
     t.join();
     
     return iLib1.getText();
+    
+    //return "ggg";
 }
