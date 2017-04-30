5acb297774eb278d3a00e297ebaaa2094c5fddb6
Changju Rhee <changju.rhee@gmail.com>
2017-05-01 03:28:59 +0900

    ATM build success

--- exes/exe1/main.cpp
+++ exes/exe1/main.cpp
@@ -1,18 +1,21 @@
 #include "../../dlls/dll1/dll1.h"
+/*
 #include <boost/log/common.hpp>
 #include <boost/log/sinks.hpp>
 #include <boost/log/sources/logger.hpp>
 #include <boost/utility/empty_deleter.hpp>
 #include <boost/shared_ptr.hpp>
+*/
 #include <iostream>
 
-using namespace boost::log;
+//using namespace boost::log;
 
 int main(int argc, char *argv[])
 {
     dll1 iDll1;
     std::cout << iDll1.getText() << std::endl;
     
+    /*
     typedef sinks::asynchronous_sink<sinks::text_ostream_backend> text_sink;
     boost::shared_ptr<text_sink> sink = boost::make_shared<text_sink>();
     
@@ -25,6 +28,7 @@ int main(int argc, char *argv[])
     
     BOOST_LOG(lg) << "note";
     sink->flush();
+     */
     
     return 0;
 }
